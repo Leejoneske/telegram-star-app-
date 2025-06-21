90b0ce371e5eab2089c01a1555b557664c40cd6a
Lee Jones KE <93858479+Leejoneske@users.noreply.github.com>
2025-06-21 13:14:29 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,21 @@
 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you're not using web vitals, you can remove this
+reportWebVitals();
+
+// Add this to satisfy isolatedModules requirement
+export {};
